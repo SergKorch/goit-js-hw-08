6776e762835381d478c310e2475cf447c8a460da
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-12 14:37:29 +0200

    HW8 p1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+const makeGalleryElements = ({ preview, original, description }) => {
+  return `<li>
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        style="display:block"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+    `;
+};
+const makeGalleryMarkup = galleryItems.map(makeGalleryElements).join("");
+const galleryElements = document.querySelector(".gallery");
+galleryElements.insertAdjacentHTML("beforeend", makeGalleryMarkup);
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
