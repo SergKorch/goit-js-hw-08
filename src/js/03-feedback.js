867283b83f1833ffd5d3dc0702aa9adfe69374af
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-13 14:53:59 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,39 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {
-  btn: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form input'),
-  message: document.querySelector('.feedback-form textarea'),
-};
-console.log(formData)
-saveInput();
-saveTextarea();
-formData.btn.addEventListener('submit', onSubmit);
-formData.email.addEventListener('input', throttle(onInput, 500));
-formData.message.addEventListener('input', throttle(onTextarea, 500));
+// const formData = {
+//   email: document.querySelector('.feedback-form input'),
+//   message: document.querySelector('.feedback-form textarea'),
+// };
 
+const form = document.querySelector('.feedback-form');
+const newSaveForm = {};
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+saveInput();
+//------------------
 function onSubmit(event) {
   event.preventDefault();
+  const formSend = new FormData(event.currentTarget);
+  formSend.forEach((value, name) => {
+    newSaveForm[name] = value;
+  });
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
+//-------------------
 function onInput(event) {
-  const email = event.target.value;
-  localStorage.setItem(STORAGE_KEY, email);
+  newSaveForm.email = form.elements.email.value;
+  newSaveForm.message = form.elements.message.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(newSaveForm));
 }
-function onTextarea(event) {
-  const message = event.target.value;
-  localStorage.setItem(STORAGE_KEY, message);
-}
+//--------------------
 function saveInput(event) {
-  const saveEmail = localStorage.getItem(STORAGE_KEY);
-  console.log(saveEmail);
-  if (saveEmail) {
-    formData.email.value = saveEmail;
-  }
-}
-function saveTextarea() {
   const saveMessage = localStorage.getItem(STORAGE_KEY);
-  console.log(saveMessage);
   if (saveMessage) {
-    formData.message.value = saveMessage;
+    const { email, message } = JSON.parse(saveMessage);
+    form.email.value = email;
+    form.message.value = message;
+    newSaveForm.email = email;
+    newSaveForm.message = message;
   }
 }
